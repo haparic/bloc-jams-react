536926ec781d2818392e5dfec5b5dd2057f0e8b2
Hugo Aparicio <hugo.aparicio8@me.com>
2019-02-23 21:37:28 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album';
 
-
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>
+      <header>
         <nav>
-           <Link to='/'>Landing</Link>
-           <Link to='/library'>Library</Link>
-           <Link to='/album'>Album</Link>
-         </nav>
-          <h1>Bloc Jams</h1>
-        </header>
+          <Link to='/'>Landing</Link>
+          <Link to='/library'>Library</Link>
+          <Link to ='/album/:slug'>Album</Link>
+
+        </nav>
+        <h1>Bloc Jams</h1>
+      </header>
         <main>
-        <Route exact path="/" component={Landing} />
-        <Route path="/library" component={Library} />
-        <Route path="/album" component={Album} />
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+          <Route path="/album/:slug" component={Album} />
         </main>
       </div>
     );
@@ -29,4 +29,3 @@ class App extends Component {
 }
 
 export default App;
-//
