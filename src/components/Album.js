5aaa0103ad483c633a99ce61c0597f6c8a3ab61a
Hugo Aparicio <hugo.aparicio8@me.com>
2019-02-23 21:37:32 -0700

    Update Album.js

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
+import albumData from './../data/albums';
 
 class Album extends Component {
+    constructor(props) {
+      super(props);
+  
+      const album = albumData.find( album => {
+        return album.slug === this.props.match.params.slug
+      });
+  
+      this.state = {
+      album: album
+    };
+  }
+
   render() {
     return (
       <section className="album">
-        {this.props.match.params.slug} Album will go here
-      </section>
+        <section id="album-info">
+          <img id="album-cover-art" src={this.state.album.albumCover} alt={this.state.album.title} />
+          <div className="album-details">
+            <h1 id="album-title">{this.state.album.title}</h1>
+            <h2 className="artist">{this.state.album.artist}</h2>
+            <div id="release-info">{this.state.album.releaseInfo}</div>
+          </div>
+        </section>
+        <table id="song-list">
+          <colgroup>
+            <col id="song-number-col" />
+            <col id="song-title-col" />
+            <col id="song-duration-col" />
+          </colgroup>
+          <tbody>
+            {this.state.album.songs.map( (song, index) =>
+              <tr className="song" key={index} >
+                <td className="song-controls">
+                </td>
+                <td className="song-title">{ song.title}</td>
+                <td className="song-duration">{"duration (s)" + song.duration }</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+         </section>
     );
   }
 }
-
-export default Album;//
+export default Album; 
