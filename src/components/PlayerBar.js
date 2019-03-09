0f92a366295d339cc2efb55adefda8bd36c51908
hugo Aparicio <hugo.aparicio8@me.com>
2019-03-08 18:25:57 -0700

    Update PlayerBar.js

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -19,18 +19,18 @@ class PlayerBar extends Component {
            </button>
          </section>
          <section id="time-control">
-         <div className="current-time">{this.props.currentTime}</div>
-           <input 
+         <div className="current-time">{this.props.formatTime(this.props.currentTime)}</div>
+          <input 
              type="range" 
              className="seek-bar" 
              value={(this.props.currentTime / this.props.duration) || 0} // || stands for or. either shows current time being played or 0
-             max="1" 
-             min="0" 
-             step="0.01" 
-             onChange={this.props.handleTimeChange}
-           />   
-           <div className="total-time">{this.props.duration}</div> 
-         </section>
+             max="1"
+            min="0"
+            step="0.01"
+            onChange={this.props.handleTimeChange}
+          />
+          <div className="total-time">{this.props.formatTime(this.props.duration)}</div>
+        </section>
          <section id="volume-control">
            <div className="icon ion-md-volume-low"></div>
            <div className="current-volume">{this.props.volume}</div>
